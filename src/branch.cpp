8159284a4658e2266fcc11a0c5ac5957edb3239c
alessandro-bugatti <alessandro.bugatti@gmail.com>
2015-10-01 12:09:17 +0200

    Fix typo

--- src/branch.cpp
+++ src/branch.cpp
@@ -8,9 +8,9 @@ using namespace std;
 Branch::Branch(Computer *c, const string& line):Instruction(c,line)
 {
    if (parameters.size() != 3)
-        throw assembly_error("Wrong numbers of parameters in ADD " + parameters_str);
+        throw assembly_error("Wrong numbers of parameters in branch " + parameters_str);
     if (!Utils::isRegisterValue(parameters.at(0)))
-        throw assembly_error("The first parameter in the brach doesn't seem a valid register: Branch " + parameters_str);
+        throw assembly_error("The first parameter in the branch doesn't seem a valid register: Branch " + parameters_str);
     if (!Utils::isRegisterValue(parameters.at(1)))
         throw assembly_error("The second parameter in the branch doesn't seem a valid address: Branch " + parameters_str);
     if (!c->containsLabel(parameters.at(2)))
