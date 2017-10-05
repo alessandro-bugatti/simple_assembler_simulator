44e8ce77ddc44152194abdc7dd2bda87c2b65470
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2017-10-05 09:04:59 +0000

    Fix formatting

--- assembly_examples/ex2.asm
+++ assembly_examples/ex2.asm
@@ -4,19 +4,19 @@
 # ha indirizzo 0x3E8.
 
 # Preload: carica i primi interi da 0x64 fino a 0x3E6
-LOADiH R1 0x00 
-LOADiL R1 0x64 
-LOADiH R2 0x03 
-LOADiL R2 0xE6 
-LOADiH R3 0x00 
-LOADiL R3 0x01 
+       LOADiH R1 0x00 
+       LOADiL R1 0x64 
+       LOADiH R2 0x03 
+       LOADiL R2 0xE6 
+       LOADiH R3 0x00 
+       LOADiL R3 0x01 
 CARICA: STORE R1 R3
-ADD R3 R3 0x1
-ADD R1 R1 0x2
-BLE R1 R2 CARICA
+       ADD R3 R3 0x1
+       ADD R1 R1 0x2
+       BLE R1 R2 CARICA
 
 #Esercizio vero e proprio
-LOADiL R1 0x64
+       LOADiL R1 0x64
        LOADiH R1 0x00
        LOADiL R2 0xE6
        LOADiH R2 0x03
