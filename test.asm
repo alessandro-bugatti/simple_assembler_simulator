26af3dd44834181545daaf1efeae5d7440d8a9c6
alessandro-bugatti <alessandro.bugatti@gmail.com>
2018-09-20 11:49:13 +0200

    Improve example

--- test.asm
+++ test.asm
@@ -1,5 +1,5 @@
 # Exercise 
-# Write a program to copy the memory content from address 0x64 until 
+# Write a program to copy the memory content from address 0x64 to
 # address 0x3E6 into the memory area starting from address 0x3E8
 
 # Preload data into memory from 0x64 to 0x3E6. 
@@ -26,7 +26,7 @@ CICLO: LOAD R4 R1
        STORE R3 R4
        ADD R1 R1 0x2
        ADD R3 R3 0x2
-       PRINT R2
+       PRINT R1
        BLE R1 R2 CICLO
        PRINT 0x490
 FINE:  END
